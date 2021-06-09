e3198aa003ec4134dafadefcec9b5bfff05f3480
yusuke <you@example.com>
2021-06-10 08:17:34 +0900

    Changed the HTML for the site.

--- mysite/from django.shortcuts import render.py
+++ mysite/from django.shortcuts import render.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html'{})
     
